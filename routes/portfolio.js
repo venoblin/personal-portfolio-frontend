7e9a4a70821f8dcfe8a01527c20afb046ad00f8b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-05-02 22:52:05 -0400

    Deleted exercise tracker project

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const exerciseRoutes = require('./exerciseTracker');
 
-router.use('/exercise-tracker', exerciseRoutes);
 router.get('/:project', (req, res) => {
   const { project } = req.params;
   res.render(`portfolio/${project}`, { project });
