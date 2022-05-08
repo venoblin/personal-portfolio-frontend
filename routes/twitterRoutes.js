7555bfa90a9674bf4cb0006300b5cdecc2e8b109
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-05-08 02:24:47 -0400

    Added post route and started delete route on the twitter clone

--- routes/twitterRoutes.js
+++ routes/twitterRoutes.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuid } = require('uuid');
 const twitterStorage = require('../utils/twitterStorage');
 const project = 'twitter-clone';
 
+
 router.get('/', (req, res) => {
     res.render(`portfolio/${project}`, { project, twitterStorage });
+});
+
+router.post('/', (req, res) => {
+    const newTweet = {
+        id: uuid(),
+        user: 'adminUser',
+        text: req.body.text
+    }
+    twitterStorage.push(newTweet);
+    res.redirect(`/portfolio/${project}`)
+});
+
+router.delete('/:id', (req, res) => {
+
 });
 
 module.exports = router;
