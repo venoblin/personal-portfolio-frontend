1a54e608b59144c63cb209fc4f00ff3c0811bbc6
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-05-25 00:45:59 -0400

    Added some styles

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,20 @@
 const headerNav = document.querySelector('header nav');
 
-let lastKnownScrollPosition = 0;
+let lastScrollTop = 0;
 document.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
-    headerNav.classList.add('nav-scroll');
+  if (window.scrollY > 75) {
+    headerNav.classList.add('nav-scroll-bg');
   } else {
-    headerNav.classList.remove('nav-scroll');
+    headerNav.classList.remove('nav-scroll-bg');
   }
+
+  const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScrollTop > lastScrollTop){
+     // downscroll code
+     headerNav.classList.add('nav-scroll-hide');
+  } else {
+     // upscroll code
+     headerNav.classList.remove('nav-scroll-hide');
+  }
+  lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
 });
