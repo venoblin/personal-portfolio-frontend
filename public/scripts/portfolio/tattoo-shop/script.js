039c92eaaa1e2b201abee51ab2923bafd195f0f9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-05-27 17:31:52 -0400

    Added comments

--- public/scripts/portfolio/tattoo-shop/script.js
+++ public/scripts/portfolio/tattoo-shop/script.js
@@ -13,122 +13,149 @@ const mainNavUl = mainNav.querySelector('ul');
 const mainNavLinks = document.querySelectorAll('.nav-links');
 
 function onNavLinks() {
-    for (let link of mainNavLinks) {
-        link.classList.remove('hide-height');
-    }
-    mainNavUl.classList.remove('hide-height');
+  for (let link of mainNavLinks) {
+    link.classList.remove('hide-height');
+  }
+  mainNavUl.classList.remove('hide-height');
 }
+
 function offNavLinks() {
-    for (let link of mainNavLinks) {
-        link.classList.add('hide-height');
-    }
-    mainNavUl.classList.add('hide-height');
+  for (let link of mainNavLinks) {
+    link.classList.add('hide-height');
+  }
+  mainNavUl.classList.add('hide-height');
 }
 
 if (window.innerWidth < 700) {
-    offNavLinks();
+  offNavLinks();
 } else {
-    onNavLinks();
+  onNavLinks();
 }
 window.addEventListener('resize', () => {
-    if (this.innerWidth < 700) {
-        offNavLinks();
-    } else {
-        onNavLinks();
-    }
+  if (this.innerWidth < 700) {
+    offNavLinks();
+  } else {
+    onNavLinks();
+  }
 });
 navLinksToggle.addEventListener('click', () => {
-    if (mainNavLinks[0].classList.contains('hide-height')) {
-        onNavLinks();
-    } else {
-        offNavLinks();
-    }
+  if (mainNavLinks[0].classList.contains('hide-height')) {
+    onNavLinks();
+  } else {
+    offNavLinks();
+  }
 });
+
 document.addEventListener('scroll', () => {
-    if (this.scrollY > 75) {
-        mainNav.classList.add('nav-color-change');
-    } else {
-        mainNav.classList.remove('nav-color-change');
-    }
-    pictureContainer.style.top = `${window.scrollY}px`;
+  //ADDS OR REMOVES NAV BACKGROUND DEPENDING ON SCROLL POSITION
+  if (this.scrollY > 75) {
+    mainNav.classList.add('nav-color-change');
+  } else {
+    mainNav.classList.remove('nav-color-change');
+  }
+
+  //MAKES SURE IMAGE VIEWER IS ALWAYS UP TOP
+  pictureContainer.style.top = `${window.scrollY}px`;
 });
 
 workersDiv.forEach((workerDiv) => {
-    const button = workerDiv.querySelector('button');
+  const button = workerDiv.querySelector('button');
+  const img = workerDiv.querySelector('img');
+
+  //HOVER EFFECT ON WORKER PROFILE PIC WHEN HOVERING OVER BUTTON
+  button.addEventListener('mouseover', () => {
     const img = workerDiv.querySelector('img');
-
-    button.addEventListener('mouseover', () => {
-        const img = workerDiv.querySelector('img');
-        img.classList.add('worker-img-hover');
-    });
-    button.addEventListener('mouseout', () => {
-        img.classList.remove('worker-img-hover');
-    });
+    img.classList.add('worker-img-hover');
+  });
+  button.addEventListener('mouseout', () => {
+    img.classList.remove('worker-img-hover');
+  });
 });
+
+//TOGGLES WORKER PORTFOLIO
 toggleGallery.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-        const tiles = workersGallery[i].querySelectorAll('.tiles');
+  btn.addEventListener('click', () => {
+    //GRABS ALL TILES AND HIDES OR SHOWS THEM
+    const tiles = workersGallery[i].querySelectorAll('.tiles');
+    for (let tile of tiles) {
+      if (tile.classList.contains('hide-height')) {
+        tile.classList.remove('hide-height');
+      } else {
+        tile.classList.add('hide-height')
+      }
+    }
 
-        for (let tile of tiles) {
-            if (tile.classList.contains('hide-height')) {
-                tile.classList.remove('hide-height');
-            } else {
-                tile.classList.add('hide-height')
-            }
-        }
-
-        if (workersGallery[i].classList.contains('worker-gallery-margin')) {
-            workersGallery[i].classList.remove('worker-gallery-margin');
-            btn.innerHTML = 'View My Work';
-        } else {
-            workersGallery[i].classList.add('worker-gallery-margin');
-            btn.innerHTML = 'Hide My Work';
-        }
-
-    });
+    //REMOVES OR ADDS TILES PARENTS MARGIN AND CHANGES BUTTON TEXT
+    if (workersGallery[i].classList.contains('worker-gallery-margin')) {
+      workersGallery[i].classList.remove('worker-gallery-margin');
+      btn.innerHTML = 'View My Work';
+    } else {
+      workersGallery[i].classList.add('worker-gallery-margin');
+      btn.innerHTML = 'Hide My Work';
+    }
+  });
 });
+
+
+/*
+IMAGE VIEWER
+*/
 let currentGallery = 0;
 let currentTile = 0;
+
 function updateImage() {
-    const tiles = workersGallery[currentGallery].querySelectorAll('.tiles');
-    const tilesImg = tiles[currentTile].querySelector('img');
+  //GRABS SELECTED WORKER GALLERY USING currentGallery
+  const tiles = workersGallery[currentGallery].querySelectorAll('.tiles');
 
-    viewerImg.src = tilesImg.src;
+  //GRABS SELECTED TILE USING currentTile
+  const tilesImg = tiles[currentTile].querySelector('img');
+
+  //IMAGE VIEWER DISPLAYS TILE IMAGE
+  viewerImg.src = tilesImg.src;
 }
-nextBtn.addEventListener('click', () => {
-    const tiles = workersGallery[currentGallery].querySelectorAll('.tiles');
 
-    if (currentTile === tiles.length - 1) {
-        currentTile = tiles.length - 1;
-    } else {
-        currentTile++
-    }
-    updateImage();
-});
-prevBtn.addEventListener('click', () => {
-    if (currentTile === 0) {
-        currentTile = 0;
-    } else {
-        currentTile--;
-    }
-    updateImage();
-});
-closeBtn.addEventListener('click', () => {
-    pictureContainer.style.display = 'none';
-    html.classList.remove('overflow-hidden');
-    currentTile = 0;
-    currentGallery = 0;
-});
 workersGallery.forEach((gallery, galleryIndex) => {
-    const tiles = gallery.querySelectorAll('.tiles');
-
-    tiles.forEach((tile, tileIndex) => {
-        tile.addEventListener('click', () => {
-            currentTile = tileIndex;
-            currentGallery = galleryIndex;
-            updateImage();
-            pictureContainer.style.display = 'flex';
-            html.classList.add('overflow-hidden');
-        });
+  const tiles = gallery.querySelectorAll('.tiles');
+  //ADDS CLICK LISTENERS TO ALL TILES FOR EACH GALLERY
+  tiles.forEach((tile, tileIndex) => {
+    tile.addEventListener('click', () => {
+      //SETS currentGallery and currentTile TO THE SELECTED GALLERY AND TILE
+      currentTile = tileIndex;
+      currentGallery = galleryIndex;
+      updateImage();
+      pictureContainer.style.display = 'flex';
+      html.classList.add('overflow-hidden');
     });
+  });
+});
+
+nextBtn.addEventListener('click', () => {
+  const tiles = workersGallery[currentGallery].querySelectorAll('.tiles');
+
+  //ADDS TO currentTile WITHOUT GOING OVER tiles.length
+  if (currentTile === tiles.length - 1) {
+    currentTile = tiles.length - 1;
+  } else {
+    currentTile++
+  }
+  updateImage();
+});
+
+prevBtn.addEventListener('click', () => {
+  //SUBTRACTS FROM currentTile WITHOUT GOING UNDER 0
+  if (currentTile === 0) {
+    currentTile = 0;
+  } else {
+    currentTile--;
+  }
+  updateImage();
+});
+
+closeBtn.addEventListener('click', () => {
+  pictureContainer.style.display = 'none';
+  html.classList.remove('overflow-hidden');
+
+  //RESETS INDEXES
+  currentTile = 0;
+  currentGallery = 0;
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,6 +8,7 @@ const workTiles = document.querySelectorAll('.work-tiles');
 const mainHeader = document.querySelector('#welcome-section h1');
 const mainSubHeader = document.querySelector('#welcome-section h2');
 
+//BURGER MENU HOVER EFFECT
 burgerMenu.addEventListener('mouseover', () => {
   burgerMenuSpans.forEach((span) => {
     span.classList.toggle('primary-color-bg');
@@ -18,77 +19,87 @@ burgerMenu.addEventListener('mouseout', () => {
     span.classList.toggle('primary-color-bg');
   });
 });
+
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('open');
   headerNav.classList.toggle('open');
   headerLinksContainer.classList.toggle('open');
+
+  //TOGGLES NAV BACKGROUND IF NAV IS AT THE TOP OF THE PAGE
   if (window.scrollY < 75) {
     headerNav.classList.toggle('nav-scroll-bg');
   }
 });
 
-
+//CHANGES TEXT BASED ON WINDOW WIDTH
 if (window.innerWidth > 800) {
-    mainHeader.textContent = 'Jonathan,';
-    mainSubHeader.textContent = 'Website Developer';
+  mainHeader.textContent = 'Jonathan,';
+  mainSubHeader.textContent = 'Website Developer';
 } else {
-    mainHeader.textContent = 'Jon,';
-    mainSubHeader.textContent = 'Web Dev';
+  mainHeader.textContent = 'Jon,';
+  mainSubHeader.textContent = 'Web Dev';
 }
 window.addEventListener('resize', () => {
-    if (this.innerWidth > 800) {
-      mainHeader.textContent = 'Jonathan,';
-      mainSubHeader.textContent = 'Website Developer';
-    } else {
-      mainHeader.textContent = 'Jon,';
-      mainSubHeader.textContent = 'Web Dev';
-    }
+  //CHANGES TEXT BASED ON RESIZE WIDTH
+  if (this.innerWidth > 800) {
+    mainHeader.textContent = 'Jonathan,';
+    mainSubHeader.textContent = 'Website Developer';
+  } else {
+    mainHeader.textContent = 'Jon,';
+    mainSubHeader.textContent = 'Web Dev';
+  }
 });
+
 let lastScrollTop = 0;
 document.addEventListener('scroll', () => {
   if (window.scrollY > 75) {
+    //ADDS NAV BACKGROUND WHEN USER SCROLLS ABOVE 75
     headerNav.classList.add('nav-scroll-bg');
   } else {
+    //REMOVES NAV BACKGROUND WHEN USER SCROLLS BELOW 75
     headerNav.classList.remove('nav-scroll-bg');
   }
 
+  //CLOSES BURGER MENU WHEN USER SCROLLS
   if (burgerMenu.classList.contains('open')) {
     burgerMenu.classList.remove('open');
     headerNav.classList.remove('open');
     headerLinksContainer.classList.remove('open');
   }
 
+  //CHECKS WHETHER USER IS SCROLLING DOWN OR UP
   const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (currentScrollTop > lastScrollTop && !burgerMenu.classList.contains('open')){
-     // downscroll code
-     headerNav.classList.add('nav-scroll-hide');
+  if (currentScrollTop > lastScrollTop && !burgerMenu.classList.contains('open')) {
+    //DOWN
+    headerNav.classList.add('nav-scroll-hide');
   } else {
-     // upscroll code
-     headerNav.classList.remove('nav-scroll-hide');
+    //UP
+    headerNav.classList.remove('nav-scroll-hide');
   }
   lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
 });
+
 
 workTiles.forEach((tile) => {
   const img = tile.querySelector('img');
   const projectInfo = tile.querySelector('.project-info');
 
+  //WORK TILES HOVER EFFECT
   tile.addEventListener('mouseover', () => {
     if (img) {
       img.classList.toggle('scale-hover');
     }
-
     projectInfo.classList.toggle('show-project-info');
   });
   tile.addEventListener('mouseout', () => {
     if (img) {
       img.classList.toggle('scale-hover');
     }
-
     projectInfo.classList.toggle('show-project-info');
   });
 });
 
+//NAV LINKS AND LOGOS HOVER EFFECT
 headerLinks.forEach((link, linkIdx) => {
   link.addEventListener('mouseover', () => {
     headerLinks.forEach((link) => {
@@ -102,7 +113,6 @@ headerLinks.forEach((link, linkIdx) => {
       }
     });
   });
-
   link.addEventListener('mouseout', () => {
     headerLinks.forEach((link) => {
       link.classList.toggle('muted-color');
