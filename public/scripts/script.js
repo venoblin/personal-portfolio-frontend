1976449a90e9f2c2fd0ff7f3e130f1fc02f3271d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-07-03 19:09:35 -0400

    Almost finished overhaul, started responsive design

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,15 @@
+const body = document.querySelector('body');
+const navToggle = document.querySelector('.nav-toggle');
+const navBar = document.querySelector('header nav');
 const mainHeader = document.querySelector('#welcome-section h1');
 const mainSubHeader = document.querySelector('#welcome-section h2');
 const arrowScrollDown = document.querySelector('.full-section .arrow-container');
+const workTiles = document.querySelectorAll('.work-grid .work-tile');
+
+navToggle.addEventListener('click', () => {
+  navBar.classList.toggle('toggle');
+  body.classList.toggle('toggle');
+});
 
 if (window.innerWidth > 800) {
   mainHeader.textContent = 'Jonathan,';
@@ -30,4 +39,23 @@ document.addEventListener('scroll', () => {
   } else {
     arrowScrollDown.classList.remove('scroll');
   }
+});
+
+workTiles.forEach((tile) => {
+  const img = tile.querySelector('img');
+  const projectInfo = tile.querySelector('.project-info');
+
+  //Work tiles hover effect
+  tile.addEventListener('mouseover', () => {
+    if (img) {
+      img.classList.toggle('hover');
+    }
+    projectInfo.classList.toggle('show');
+  });
+  tile.addEventListener('mouseout', () => {
+    if (img) {
+      img.classList.toggle('hover');
+    }
+    projectInfo.classList.toggle('show');
+  });
 });
