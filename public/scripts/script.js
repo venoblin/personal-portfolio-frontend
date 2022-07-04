797cc34e520141ec7c53611a17e01a489789663f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-07-04 02:37:27 -0400

    Finished overhaul

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,40 @@
 const body = document.querySelector('body');
 const navToggle = document.querySelector('.nav-toggle');
 const navBar = document.querySelector('header nav');
+const navLinks = navBar.querySelectorAll('.link');
 const mainHeader = document.querySelector('#welcome-section h1');
 const mainSubHeader = document.querySelector('#welcome-section h2');
 const arrowScrollDown = document.querySelector('.full-section .arrow-container');
 const workTiles = document.querySelectorAll('.work-grid .work-tile');
 
 navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('toggle');
   navBar.classList.toggle('toggle');
   body.classList.toggle('toggle');
+});
+
+navLinks.forEach(link => {
+
+  link.addEventListener('click', () => {
+    if (window.innerWidth < 800) {
+      if (navBar.classList.contains('toggle')) {
+        navToggle.classList.remove('toggle');
+        navBar.classList.remove('toggle');
+        body.classList.remove('toggle');
+      }
+    }
+  });
 });
 
 if (window.innerWidth > 800) {
   mainHeader.textContent = 'Jonathan,';
   mainSubHeader.textContent = 'Website Developer';
+
+  if (navBar.classList.contains('toggle')) {
+    navToggle.classList.remove('toggle');
+    navBar.classList.remove('toggle');
+    body.classList.remove('toggle');
+  }
 } else {
   mainHeader.textContent = 'Jon,';
   mainSubHeader.textContent = 'Web Dev';
@@ -22,6 +43,12 @@ window.addEventListener('resize', () => {
   if (this.innerWidth > 800) {
     mainHeader.textContent = 'Jonathan,';
     mainSubHeader.textContent = 'Website Developer';
+
+    if (navBar.classList.contains('toggle')) {
+      navToggle.classList.remove('toggle');
+      navBar.classList.remove('toggle');
+      body.classList.remove('toggle');
+    }
   } else {
     mainHeader.textContent = 'Jon,';
     mainSubHeader.textContent = 'Web Dev';
