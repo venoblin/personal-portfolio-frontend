086d78a3e5448c51da212772a2d129edc3c511a9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-07-19 16:31:45 -0400

    Fixed typo in styles.css

--- seeds/generateUsers.js
+++ seeds/generateUsers.js
@@ -6,8 +6,8 @@ function generateUsers(amount, maxTweets) {
   //Generates random users with a random username
   for (let i = 1; i < amount; i++) {
 
-    const randAdj = Math.floor(Math.random() * (adjectives.length - 1));
-    const randNou = Math.floor(Math.random() * (nouns.length - 1));
+    const randAdj = Math.floor(Math.random() * adjectives.length);
+    const randNou = Math.floor(Math.random() * nouns.length);
 
     const username = `${adjectives[randAdj]}${nouns[randNou]}`;
 
@@ -23,7 +23,7 @@ function generateUsers(amount, maxTweets) {
     if (user.id.length !== 1) {
       const randAmount = Math.floor(Math.random() * maxTweets) + 1;
       for (let i = 0; i < randAmount; i++) {
-        const randTweet = Math.floor(Math.random() * (statements.length - 1));
+        const randTweet = Math.floor(Math.random() * statements.length);
         twitterTweets.push({
           id: uuid(),
           text: statements[randTweet],
