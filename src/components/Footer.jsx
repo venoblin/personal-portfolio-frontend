ffced76a6087a6037d9e95400b431fdd1f4859d2
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-24 13:24:37 -0500

    Added to footer and changed box shadow for navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import '../styles/Footer.css'
 const Footer = () => {
   return (
     <footer className="Foot">
+      <p>Created With React</p>
       <p>Jon / Web Dev © 2022</p>
     </footer>
   )
