3123ba5927e71f0675c1ef7cb14c5b79fec81319
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-25 18:39:59 -0500

    Added credits to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ const Footer = () => {
   return (
     <footer className="Foot">
       <p>Created With React</p>
+      <p>Icons From Font Awesome</p>
+      <p>Fonts From Google Fonts</p>
       <p>Jon / Web Dev © 2022</p>
     </footer>
   )
