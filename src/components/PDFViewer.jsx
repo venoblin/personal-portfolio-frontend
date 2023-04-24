4a99e4ece201aa81aecbabd5f59ac677a864fb00
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-04-23 20:26:29 -0400

    Added react-router-dom dependency

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -1,4 +1,5 @@
 import '../styles/PDFViewer.css'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
 import { useState } from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 
@@ -10,10 +11,22 @@ const PDFViewer = () => {
     setNumPages(numPage)
   }
 
+  const zoomOutHandler = () => {
+
+  }
+
+  const zoomInHandler = () => {
+
+  }
+
   return ( 
     <div className='PDFViewer'>
       <Document file='/certificate.pdf' onLoadSuccess={onDocumentLoad}>
-        <Page pageNumber={pageNumber} />
+        <div className='inputs'>
+          <button onClick={zoomOutHandler}>OUT</button>
+          <button onClick={zoomInHandler}>IN</button>
+        </div>
+        <Page scale={1} pageNumber={pageNumber} />
       </Document>
     </div>
   )
