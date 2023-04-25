85875f16dcf082444dabbc7c835750ba5fcad9e6
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-04-25 03:17:15 -0400

    Added main component to home route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css'
 import { Routes, Route } from 'react-router-dom'
+import Main from './components/Main'
 import PDFViewer from './components/PDFViewer'
 import Error from './components/Error'
 import Footer from './components/Footer'
@@ -8,12 +9,7 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={
-          
-          }
-        />
+        <Route path="/" element={<Main />} />
         <Route path="/viewer/:pdfName" element={<PDFViewer />} />
         <Route path="*" element={<Error code="404" msg="Not Found :(" />} />
       </Routes>
