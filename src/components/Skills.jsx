f799d2477d2998553f2200547b96826ff0acf5d6
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-22 02:18:26 -0500

    fixed svg

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ const Skills = () => {
     <div className="Skills">
 
       {skills.map(skill => (
-        <div className='skill'>
+        <div className='skill' key={skill.name}>
           {skill.svg}
           <p className='name'>{skill.name}</p>
         </div>
