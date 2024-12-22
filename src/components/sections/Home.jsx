8f32ad36ce8c80b2dccb3e2f859026cbe5ae19ed
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-22 00:03:41 -0500

    modified home layout

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,29 +5,31 @@ import Skills from '../Skills'
 const Home = () => {
   return (
     <section className="Home">
-      <div className='emoji-wrapper'>
-        <img className="emoji" src={emojiTyping} alt="Emoji typing" />
+      <div className='home-wrapper'>
+        <div className='emoji-wrapper'>
+          <img className="emoji" src={emojiTyping} alt="Emoji typing" />
+        </div>
+        
+        <div>
+          <h1 className="intro-heading">
+            Hello there!{' '}
+            <span className="wrapper">
+              I'm <span className="highlight">Jonathan</span>,
+            </span>
+            <span className="wrapper">
+              a <span className="highlight">Full-Stack Engineer</span>!
+            </span>
+          </h1>
+          <a
+            className="btn"
+            href="https://www.linkedin.com/in/jonathanvillagomezhernandez/"
+            target="_blank"
+          >
+            Let's Connect!
+          </a>
+        </div>
+        <Skills />
       </div>
-      
-      <div>
-        <h1 className="intro-heading">
-          Hello there!{' '}
-          <span className="wrapper">
-            I'm <span className="highlight">Jonathan</span>,
-          </span>
-          <span className="wrapper">
-            a <span className="highlight">Full-Stack Engineer</span>!
-          </span>
-        </h1>
-        <a
-          className="btn"
-          href="https://www.linkedin.com/in/jonathanvillagomezhernandez/"
-          target="_blank"
-        >
-          Let's Connect!
-        </a>
-      </div>
-      <Skills />
     </section>
   )
 }
