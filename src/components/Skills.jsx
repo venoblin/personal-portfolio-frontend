9f9fc5e8eae9f97a1a03ef83bddfa5c4bb597f85
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-22 23:47:06 -0500

    added code block

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,14 +5,22 @@ const Skills = () => {
 
   return (
     <div className="Skills">
+      <p className='command'>
+        <span className='keyword'>void </span> 
+        <span className='name'>displayTechnologies </span> 
+        <span className='braces'>{`() {`}</span>
+      </p>
 
-      {skills.map(skill => (
-        <div className='skill' key={skill.name}>
-          {skill.svg}
-          <p className='name'>{skill.name}</p>
-        </div>
-      ))}
+      <div className='techs'>
+        {skills.map(skill => (
+          <div className='skill' key={skill.name}>
+            {skill.svg}
+            <p className='name'>{skill.name}</p>
+          </div>
+        ))}
+      </div>
     
+      <p className='command'><span className='braces'>{`}`}</span></p>
     </div>
   )
 }
