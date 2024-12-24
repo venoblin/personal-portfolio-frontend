d5c6a2ee55dad9bed00372c1102074fa6f3df1e3
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-23 23:41:05 -0500

    worked on switcher

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,6 +6,7 @@ import RadioButtons from '../RadioButtons'
 
 const Projects = () => {
   const [projectCategory, setProjectCategory] = useState('featured')
+  const [isHoverActive, setIsHoverActive] = useState(true)
   const categories = [
     'featured', 
     'web', 
@@ -17,27 +18,35 @@ const Projects = () => {
     setProjectCategory(category)
   }
 
+  const mouseEnterHandler = () => {
+    setIsHoverActive(true)
+  }
+  
+  const mouseLeaveHandler = (evt) => {
+    setIsHoverActive(false)
+  }
+  
   return (
     <section className="Projects" id="projects">
       
       <div className="wrapper">
-        <div className='h2-switcher'>
-          <h2>
-            <span className='category'>
-              {projectCategory}
-              <span className='indicator'>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
-	                <path d="M5.143 9.847a1 1 0 0 0 1.715 0l3.999-6.665a1 1 0 0 0-.858-1.515H2.001a1 1 0 0 0-.858 1.515z" />
-                </svg>
-              </span>
-            </span> Projects
-          </h2>
+        <h2>
+          <span className={isHoverActive ? 'category active' : 'category'} onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+            {projectCategory}
+            <span className='indicator'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
+                <path d="M5.143 9.847a1 1 0 0 0 1.715 0l3.999-6.665a1 1 0 0 0-.858-1.515H2.001a1 1 0 0 0-.858 1.515z" />
+              </svg>
+            </span>
+          </span> Projects
+        </h2>
 
-          <div className='switcher'>
+        <div className='switcher' onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+          <div className={isHoverActive ? 'categories active' : 'categories'}>
             {categories.filter((category) => {
               return category !== projectCategory
             }).map((category) => (
-              <p className='category' key={category} onClick={() => switchCategory(category)}>{category}</p>
+              <button className='category' key={category} onClick={() => switchCategory(category)}>{category}</button>
             ))}
           </div>
         </div>
