c6b1b5c532d5b9d6350115d9f3ba5530a6df7f03
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-02-07 23:12:20 -0500

    added comptia a plus cert

--- src/components/sections/Certifications.jsx
+++ src/components/sections/Certifications.jsx
@@ -1,10 +1,19 @@
 import '../../styles/Certifications.css'
+import comptiaAPng from '../../assets/images/comptia-a+.png'
+import comptiaACert from '../../assets/documents/ComptiaACertificate.pdf'
 
 const Certifications = () => {
   return (
     <section className='Certifications' id='certifications'>
       <div className='wrapper'>
         <h2>Certifications</h2>
+
+        <div className='all-certs'>
+          <div className='cert'>
+            <img src={comptiaAPng} alt='CompTIA A+' />
+            <a className='btn hollow' href={comptiaACert} target="_blank">View</a>
+          </div>
+        </div>
       </div>
     </section>
   )
