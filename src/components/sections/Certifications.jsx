5bfed8a3e20fc7d293bcb84c6874d156699ff4aa
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-02-07 22:51:51 -0500

    created certifications component

---
+++ src/components/sections/Certifications.jsx
@@ -0,0 +1,13 @@
+import '../../styles/Certifications.css'
+
+const Certifications = () => {
+  return (
+    <section className='Certifications' id='certifications'>
+      <div className='wrapper'>
+        <h2>Certifications</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Certifications
