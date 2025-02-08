1f9fb1859c5b5ad74cc25d71eedb9c3bb4317e66
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-02-07 23:37:04 -0500

    added skills

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -9,25 +9,32 @@ const Skills = () => {
       <div className='wrapper'>
         <h2>Skills</h2>
         
-        <div className='code-block'>
-          <p className='command'>
-            <span className='keyword'>void </span> 
-            <span className='name'>displayTechnologies </span> 
-            <span className='braces'>{`( ) {`}</span>
-          </p>
-
-          <div className='techs'>
-            {skills.map(skill => (
-              <div className='skill' key={skill.name}>
-                {skill.svg}
-                <p className='name'>{skill.name}</p>
-              </div>
-            ))}
+        <div className='all-skills'>
+          <div className='skill'>
+            <h3>Hardware</h3>
+            <p>Installation, troubleshooting, maintenance, computers, laptops, mobile devices, printers, servers, 
+              storage devices, peripherals</p>
           </div>
-      
-          <p className='command'><span className='braces'>{`}`}</span></p>
+          
+          <div className='skill'>
+            <h3>Software</h3>
+            <p>
+              Troubleshooting, system configuration, Windows, MacOS, Linux, iOS, Android, OS installation, malware
+            removal, virtualization, backup/recovery, scripting (Bash, Python)</p>
+          </div>
+
+          <div className='skill'>
+            <h3>Networking</h3>
+            <p>Configuration, routing, switching, access points, firewalls, VPNs, TCP/IP, DNS, DHCP, network security,
+            troubleshooting</p>
+          </div>
+
+          <div className='skill'>
+            <h3>Tools</h3>
+            <p>Windows System Tools, Command Prompt, Terminal, TigerVNC, Oracle VirtualBox, Google Workspace (Docs,
+              Sheets, Gmail), Slack, Microsoft Teams, Zoom</p>
+          </div>
         </div>
-        
       </div>
     </section>
   )
