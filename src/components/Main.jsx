7b0cd11047fa32c852c0980a07838965c10fc3f9
Jonathan <villagomezhjonathan@gmail.com>
2026-03-11 15:57:18 -0400

    made a loop to programatically make certs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,6 @@ import Contact from './sections/Contact'
 import Skills from './sections/Skills'
 import Certifications from './sections/Certifications'
 
-
 const Main = () => {
   const [scrollState, setScrollState] = useState({
     isScrolling: false,
@@ -40,29 +39,29 @@ const Main = () => {
       }
     })
   }, [])
-  
+
   return (
     <div>
       <NavBar scrollState={scrollState} isMobile={isMobile} />
 
       <main>
         <Home scrollState={scrollState} />
-        <div className='sections'>
-          <Certifications />
+        <div className="sections">
           <Skills />
           <Projects isMobile={isMobile} />
           <About />
+          <Certifications />
           <Contact />
-          <div className='border'>
+          <div className="border">
             <svg
               preserveAspectRatio="none"
               viewBox="0 0 1200 120"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-              d="M0 0v46.29c47.79 22.2 103.59 32.17 158 28 70.36-5.37 136.33-33.31 206.8-37.5 73.84-4.36 147.54 16.88 218.2 35.26 69.27 18 138.3 24.88 209.4 13.08 36.15-6 69.85-17.84 104.45-29.34C989.49 25 1113-14.29 1200 52.47V0z"
-              opacity=".25"
-            />
+                d="M0 0v46.29c47.79 22.2 103.59 32.17 158 28 70.36-5.37 136.33-33.31 206.8-37.5 73.84-4.36 147.54 16.88 218.2 35.26 69.27 18 138.3 24.88 209.4 13.08 36.15-6 69.85-17.84 104.45-29.34C989.49 25 1113-14.29 1200 52.47V0z"
+                opacity=".25"
+              />
               <path
                 d="M0 0v15.81c13 21.11 27.64 41.05 47.69 56.24C99.41 111.27 165 111 224.58 91.58c31.15-10.15 60.09-26.07 89.67-39.8 40.92-19 84.73-46 130.83-49.67 36.26-2.85 70.9 9.42 98.6 31.56 31.77 25.39 62.32 62 103.63 73 40.44 10.79 81.35-6.69 119.13-24.28s75.16-39 116.92-43.05c59.73-5.85 113.28 22.88 168.9 38.84 30.2 8.66 59 6.17 87.09-7.5 22.43-10.89 48-26.93 60.65-49.24V0z"
                 opacity=".5"
@@ -72,7 +71,8 @@ const Main = () => {
           </div>
         </div>
       </main>
-    </div>)
+    </div>
+  )
 }
 
 export default Main
--- src/components/sections/Certifications.jsx
+++ src/components/sections/Certifications.jsx
@@ -1,18 +1,33 @@
 import '../../styles/Certifications.css'
-import comptiaAPng from '../../assets/images/comptia-a+.png'
-import comptiaACert from '../../assets/documents/ComptiaACertificate.pdf'
+import comptiaAImg from '../../assets/images/a+.svg'
+import comptiaAPdf from '../../assets/documents/ComptiaACertificate.pdf'
+import comptiaNetworkImg from '../../assets/images/network+.svg'
+import comptiaNetworkPdf from '../../assets/documents/ComptiaNetwork.pdf'
 
 const Certifications = () => {
+  const certs = [
+    {
+      name: 'CompTIA Network+',
+      img: comptiaNetworkImg,
+      pdf: comptiaNetworkPdf
+    },
+    { name: 'CompTIA A+', img: comptiaAImg, pdf: comptiaAPdf }
+  ]
+
   return (
-    <section className='Certifications' id='certifications'>
-      <div className='wrapper'>
+    <section className="Certifications" id="certifications">
+      <div className="wrapper">
         <h2>Certifications</h2>
 
-        <div className='all-certs'>
-          <div className='cert'>
-            <img src={comptiaAPng} alt='CompTIA A+' />
-            <a className='btn hollow' href={comptiaACert} target="_blank">View</a>
-          </div>
+        <div className="all-certs">
+          {certs.map((c) => (
+            <div className="cert" key={c.name}>
+              <img src={c.img} alt={c.name} />
+              <a className="btn hollow" href={c.pdf} target="_blank">
+                View
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </section>
