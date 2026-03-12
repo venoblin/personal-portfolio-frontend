68e80683c500312f5d5e906721c3b93b49e8852b
Jonathan <villagomezhjonathan@gmail.com>
2026-03-12 02:01:15 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Main from './components/Main'
 import NotFound from './components/NotFound'
 import Footer from './components/Footer'
+import { useContext, useEffect } from 'react'
+import { AppContext } from './contexts/AppContext'
 
 const App = () => {
   return (
