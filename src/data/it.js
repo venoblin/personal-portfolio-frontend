ad9b09e746a508134373c93086942c27add661d4
Jonathan <villagomezhjonathan@gmail.com>
2026-03-13 16:47:14 -0400

    finished it portfolio

--- src/data/it.js
+++ src/data/it.js
@@ -28,7 +28,12 @@ const it = {
   ),
   projects: foundProjects,
   certifications: certifications,
-  techStack: [techStack.python],
+  techStack: [
+    techStack.python,
+    techStack.bash,
+    techStack.powerShell,
+    techStack.sql
+  ],
   skills: [
     {
       svg: (
@@ -39,13 +44,9 @@ const it = {
       header: 'Hardware',
       list: [
         'Installation, Troubleshooting, and Maintenance',
-        'Computers',
-        'Laptops',
-        'Mobile Devices',
-        'Printers',
-        'Servers',
-        'Storage Devices',
-        'Peripherals'
+        'Desktops, Laptops, Servers',
+        'Storage Arrays & Network Devices',
+        'Fiber & Copper Cabling'
       ]
     },
     {
@@ -54,20 +55,11 @@ const it = {
           <path d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm0-2h16V8H4zm4.675-5l-1.9-1.9q-.3-.3-.288-.7t.313-.7q.3-.275.7-.287t.7.287l2.6 2.6q.3.3.3.7t-.3.7l-2.6 2.6q-.275.275-.687.288T6.8 16.3q-.275-.275-.275-.7t.275-.7zM13 17q-.425 0-.712-.288T12 16t.288-.712T13 15h4q.425 0 .713.288T18 16t-.288.713T17 17z"></path>
         </svg>
       ),
-      header: 'Software',
+      header: 'Software & OS',
       list: [
-        'Installation and Troubleshooting',
-        'System Configuration',
-        'Windows',
-        'MacOS',
-        'Linux',
-        'iOS',
-        'Android',
-        'OS Installation',
-        'Malware Removal',
-        'Virtualization',
-        'Backup/Recovery',
-        'Scripting (Bash, Python)'
+        'Windows, macOS, Linux',
+        'OS Installation, System Configuration, Virtualization',
+        'Backup & Recovery'
       ]
     },
     {
@@ -78,14 +70,9 @@ const it = {
       ),
       header: 'Networking',
       list: [
-        'Troubleshooting',
-        'Configuration',
-        'Routing',
-        'Switching',
-        'Access Points',
-        'Firewalls',
-        'VPNs, TCP/IP, DNS, and DHCP',
-        'Network Security'
+        'Troubleshooting & Configuration ',
+        'TCP/IP, DNS, DHCP, VPN',
+        'Routing, Switching, Firewalls, Network Security'
       ]
     },
     {
@@ -94,14 +81,13 @@ const it = {
           <path d="M7 23q-.5-1-.75-2.45T6 17.6q0-2.475.55-4.088T8 11V6l3-5h.5v5.15q-.25.125-.375.35T11 7q0 .425.288.713T12 8t.713-.288T13 7q0-.275-.125-.5t-.375-.35V1h.5l3 5v5q.925.9 1.463 2.513T18 17.6q0 1.5-.25 2.95T17 23q-1.05-.325-1.675-1.187t-.6-1.963q0-.625.138-1.35T15 17.05q0-1.45-.788-3.05T12 11q-1.4 1.4-2.2 3T9 17.05q0 .725.15 1.45t.175 1.35q0 1.1-.637 1.975T7 23"></path>
         </svg>
       ),
-      header: 'Tools',
+      header: 'Tools & Productivity',
       list: [
-        'Windows System Tools',
-        'Command Prompt and Terminal',
-        'TigerVNC',
-        'Oracle VirtualBox',
-        'Google Workspace (Docs, Sheets, Gmail)',
-        'Slack, Microsoft Teams, and Zoom'
+        'ServiceNow, Jira',
+        'Docker, Oracle VirtualBox',
+        'TigerVNC, Remmina',
+        'Microsoft Office, Google Workspace',
+        'Microsoft Teams, Zoom, Slack'
       ]
     }
   ]
